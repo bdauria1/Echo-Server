26cc44c99b814c496732993ff253e39fd9339e76
bdauria1 <103322615+bdauria1@users.noreply.github.com>
2023-10-12 19:24:55 -0400

    put in new files

--- Client.py
+++ Client.py
@@ -1,15 +1,60 @@
-import socket
-import socket, sys
+import socket, sys, time
 
-HOST = sys.argv[1]
-PORT = int(sys.argv[2])
+def run_test(host, port, measurement_type, num_probes, msg_size, server_delay):
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client_socket.connect((host, port))
 
-client_socket.connect((HOST, PORT))
+    # Connection Setup Phase
 
-client_socket.send('Hello, server!'.encode('utf-8'))
-data = client_socket.recv(1024)
-print(f'Received from server: {data.decode("utf-8")}')
+    setup_message = f's {measurement_type} {num_probes} {msg_size} {server_delay}\n'
 
-client_socket.close()
+    client_socket.send(setup_message.encode('utf-8'))
+    setup_response = client_socket.recv(1024).decode("utf-8")
+    print(f'Received from server: {setup_response}')
+
+    # Checking for a 200 response
+    if setup_response[0:3] != "200":
+        client_socket.close()
+        exit()
+
+    # Measurement Phase
+    payload = '?' * msg_size
+    rtt_vals = []
+    tput_vals = []
+    for probe_sequence_number in range(1, num_probes + 1):
+        probe_message = f"m {probe_sequence_number} {payload}"
+        begin_time = time.perf_counter_ns()
+        client_socket.send(probe_message.encode("utf-8"))
+        probe_response = client_socket.recv(msg_size + 128).decode("utf-8")
+        end_time = time.perf_counter_ns()
+        rtt = end_time - begin_time
+        if measurement_type == "rtt":
+            rtt_vals.append(rtt)
+            print(f"Current RTT: {rtt/1000000} ms.")
+        else:
+            tput = 2 * len(probe_message) / rtt
+            tput_vals.append(tput)
+            print(f"Current TPUT: {tput * 8000000} kbps")
+        
+    if measurement_type == 'rtt':
+        average = (sum(rtt_vals)/len(rtt_vals))/1000000
+        print(f"The average {measurement_type} value is {(sum(rtt_vals)/len(rtt_vals))/1000000} ms.")
+    else:
+        average = (sum(tput_vals)/len(tput_vals))*8000000
+        print(f"The average {measurement_type} value is {(sum(tput_vals)/len(tput_vals))*8000000} kbps")
+
+    # Termination Phase
+    client_socket.send("t\n".encode("utf-8"))
+    termination_response = client_socket.recv(1024).decode("utf-8")
+    print(f"Received from server: {termination_response}")
+    print("Terminating connection...")
+    client_socket.close()
+    return average
+    
+
+
+if __name__ == "__main__":
+    host = sys.argv[1]
+    port = int(sys.argv[2])
+    run_test(host, port, 'rtt', 10, 10, 0)
--- Server.py
+++ Server.py
@@ -1,3 +1,51 @@
+import socket, sys, time
+
+MEASUREMENT_TYPE = ''
+NUM_PROBES = 0
+MESSAGE_SIZE = 0
+SERVER_DELAY = -1
+PROBE_SEQUENCE_NUMBER = 0
+
+def parse_setup_message(msg: str):
+    msg_info = msg.split(' ')
+    if len(msg_info) != 5:
+        raise ValueError("Incorrect setup message")
+    phase = msg_info[0]
+    measurement_type = msg_info[1]
+    num_probes = int(msg_info[2])
+    message_size = int(msg_info[3])
+    server_delay = float(msg_info[4].strip())
+    if phase != 's':
+        raise ValueError(f"Invalid phase: {phase}")
+    if measurement_type not in ['rtt', 'tput']:
+        raise ValueError(f"Invalid mesurement type: {measurement_type}")
+    if num_probes <= 0:
+        raise ValueError(f"Invalid number of probes: {num_probes}")
+    if message_size <= 0:
+        raise ValueError(f"Invalid message size: {message_size}")
+    if server_delay < 0:
+        raise ValueError(f"Invalid server delay: {server_delay}")
+    return measurement_type, num_probes, message_size, server_delay
+
+
+def parse_probe_message(msg: str, current_probe):
+    msg_info = msg.split(' ')
+    if len(msg_info) != 3:
+        raise ValueError("Incorrect setup message")
+    phase = msg_info[0]
+    probe_seq_num = int(msg_info[1])
+    payload = msg_info[2]
+
+    if phase != 'm':
+        raise ValueError(f"Incorrect phase: {phase}")
+    if probe_seq_num != current_probe:
+        raise ValueError(f"Incorrect probe sequence number: {probe_seq_num}")
+    if len(payload) != MESSAGE_SIZE:
+        raise ValueError(f"Incorrect payload size")
+    
+    return 
+
+
 PORT = int(sys.argv[1])
 
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -7,19 +55,46 @@ server_socket.bind((host, PORT))
 
 server_socket.listen(5)
 
+
 print(f'Server started on {host}:{PORT}')
 print('Waiting for a connection...')
 
 while True:
+    # Connection Setup Phase
     client_socket, addr = server_socket.accept()
     print(f'Connection established with: {addr}')
 
-    data = client_socket.recv(1024)
-    if not data:
-        print(f'Client {addr} disconnected')
+    setup_message = client_socket.recv(1024)
+    try:
+        MEASUREMENT_TYPE, NUM_PROBES, MESSAGE_SIZE, SERVER_DELAY = parse_setup_message(setup_message.decode('utf-8'))
+        client_socket.send("200 OK: Ready".encode("utf-8"))
+        print(f'Setup Message received from client: {setup_message.decode("utf-8")}')
+    except Exception as e:
+        print(f"Error: {e}")
+        client_socket.send("404 ERROR: Invalid Connection Setup Message".encode("utf-8"))
         client_socket.close()
         continue
-    
-    print(f'Received from client: {data.decode("utf-8")}')
-    client_socket.send(data)
+
+
+    # Measurement Phase
+    for probe_sequence_num in range(1, NUM_PROBES+1):
+        probe_message = client_socket.recv(MESSAGE_SIZE + 128)
+        try:
+            parse_probe_message(probe_message.decode("utf-8"), probe_sequence_num)
+            time.sleep(SERVER_DELAY)
+            client_socket.send(probe_message)
+            print(f'Probe Message received from client: {probe_message.decode("utf-8")}')
+        except Exception as e:
+            print(f"Error: {e}")
+            client_socket.send("404 ERROR: Invalid Measurement Message".encode("utf-8"))
+            client_socket.close()
+            continue
+
+    # Termination Phase
+    term_message = client_socket.recv(1024).decode("utf-8")
+    if term_message == "t\n":
+        client_socket.send("200 OK: Closing Connection".encode("utf-8"))
+    else:
+        client_socket.send("404 ERROR: Invalid Connection Termination Message".encode("utf-8"))
+
     client_socket.close()
